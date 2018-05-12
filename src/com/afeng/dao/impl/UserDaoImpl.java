18327671fcf9782e269ed53e5ad419bf956820d7
Chen Feng <afeng3990@163.com>
2018-05-12 17:07:26 +0800

    #继续练习

--- src/com/afeng/dao/impl/UserDaoImpl.java
+++ src/com/afeng/dao/impl/UserDaoImpl.java
@@ -1,7 +1,9 @@
 package com.afeng.dao.impl;
 
 import com.afeng.dao.UserDao;
+import org.springframework.stereotype.Component;
 
+@Component(value = "userDao")
 public class UserDaoImpl implements UserDao {
     @Override
     public void sayHello() {
